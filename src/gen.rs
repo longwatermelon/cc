402db6a63a45ea4246d4c9a7555a1c48cacc3e58
longwatermelon <longwatermelon1@gmail.com>
2023-06-25 13:10:20 -0700

    begin asm gen

---
+++ src/gen.rs
@@ -0,0 +1,54 @@
+use crate::error::Error;
+use crate::node::{Node, NodeVariant};
+
+pub struct Gen {
+    data: String
+}
+
+impl Gen {
+    pub fn new() -> Self {
+        Self { data: String::new() }
+    }
+
+    pub fn gen(&mut self, root: &Node) -> Result<String, Error> {
+        self.data = String::from("section .rodata\n");
+        let start: String = String::from("global _start\nsection .text\n_start:\ncall main\nmov rdi, rax\nmov rax, 60\nsyscall\n\n");
+        let body: String = self.gen_expr(root)?;
+
+        Ok(format!("{}{}\n{}", start, body, self.data))
+    }
+
+    fn gen_expr(&mut self, n: &Node) -> Result<String, Error> {
+        match n.variant.as_ref() {
+            NodeVariant::Cpd {..} => self.gen_cpd(n),
+            NodeVariant::Fdef {..} => self.gen_fdef(n),
+            NodeVariant::Return {..} => self.gen_return(n),
+            NodeVariant::Int { value } => Ok(value.to_string()),
+            _ => todo!()
+        }
+    }
+
+    fn gen_cpd(&mut self, n: &Node) -> Result<String, Error> {
+        let NodeVariant::Cpd { values } = n.variant.as_ref() else { panic!() };
+        let mut res: String = String::new();
+
+        for n in values {
+            res.push_str(self.gen_expr(n)?.as_str());
+        }
+
+        Ok(res)
+    }
+
+    fn gen_fdef(&mut self, n: &Node) -> Result<String, Error> {
+        let NodeVariant::Fdef { name, params: _, body, rtype: _ } = n.variant.as_ref() else { panic!() };
+        let res: String = format!("{}:\npush rbp\nmov rbp, rsp\n{}\nmov rsp, rbp\npop rbp\nret\n\n", name, self.gen_expr(body)?);
+
+        Ok(res)
+    }
+
+    fn gen_return(&mut self, n: &Node) -> Result<String, Error> {
+        let NodeVariant::Return { value } = n.variant.as_ref() else { panic!() };
+        Ok(format!("mov rax, {}", self.gen_expr(value)?))
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,25 @@ mod lexer;
 mod node;
 mod parser;
 mod preprocess;
+mod gen;
 
 use error::Error;
 use parser::Parser;
 use node::Node;
 use preprocess::Preprocessor;
+use gen::Gen;
 use std::fs;
+use std::io::Write;
+use std::process::Command;
+
+fn handle_err<T>(res: Result<T, Error>) -> T {
+    if let Err(e) = res {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    } else {
+        res.unwrap()
+    }
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
@@ -17,17 +30,25 @@ fn main() {
         std::process::exit(1);
     }
 
+    // Preprocessor
     let prog: String = fs::read_to_string(args[0].as_str()).expect("Couldn't read file examples/test.c.");
     let mut preprocessor: Preprocessor = Preprocessor::new(prog);
     preprocessor.preprocess();
     let processed: String = preprocessor.result();
 
+    // Parser
     let mut parser: Parser = Parser::new(processed).unwrap();
-    let root: Result<Node, Error> = parser.parse();
+    let root: Node = handle_err(parser.parse());
 
-    match root {
-        Ok(x) => println!("{:#?}", x),
-        Err(e) => println!("{}", e)
-    }
+    // Assembly generation
+    let mut generator: Gen = Gen::new();
+    let result: String = handle_err(generator.gen(&root));
+
+    // Write to file
+    let mut f = fs::File::create("a.s").expect("Unable to create file 'a.s'.");
+    f.write_all(result.as_bytes()).expect("Unable to write to file 'a.s'.");
+
+    Command::new("sh").args(&["-c", "nasm -felf64 a.s && ld *.o"]).output().unwrap();
+    fs::remove_file("a.o").unwrap();
 }
 
